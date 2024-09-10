bac88b0a2a84a73ef5a2ebeb32dc9539720c3154
Nikita Yutanov <nikitayutanov@gmail.com>
2024-09-10 18:28:11 +0300

    Add selective wallet connect

--- src/consts.ts
+++ src/consts.ts
@@ -2,9 +2,4 @@ const ADDRESS = {
   NODE: import.meta.env.VITE_NODE_ADDRESS as string,
 };
 
-const LOCAL_STORAGE = {
-  ACCOUNT: 'account',
-  WALLET: 'wallet',
-};
-
-export { ADDRESS, LOCAL_STORAGE };
+export { ADDRESS };
--- src/features/wallet/components/wallet-modal/WalletModal.tsx
+++ src/features/wallet/components/wallet-modal/WalletModal.tsx
@@ -18,52 +18,51 @@ type Props = {
 };
 
 function WalletModal({ onClose }: Props) {
-  const { extensions, account, login, logout } = useAccount();
+  const { wallets, isAnyWallet, account, login, logout } = useAccount();
   const alert = useAlert();
 
-  const { wallet, walletAccounts, setWalletId, resetWalletId, getWalletAccounts, saveWallet, removeWallet } =
-    useWallet();
+  const { wallet, walletAccounts, setWalletId, resetWalletId } = useWallet();
 
-  const getWallets = () =>
+  const renderWallets = () =>
     WALLETS.map(([id, { SVG, name }]) => {
-      const isEnabled = extensions?.some((extension) => extension.name === id);
-      const status = isEnabled ? 'Enabled' : 'Disabled';
+      const { status, accounts, connect } = wallets?.[id] || {};
+      const isEnabled = Boolean(status);
+      const isConnected = status === 'connected';
 
-      const accountsCount = getWalletAccounts(id)?.length;
+      const accountsCount = accounts?.length;
       const accountsStatus = `${accountsCount} ${accountsCount === 1 ? 'account' : 'accounts'}`;
-
-      const onClick = () => setWalletId(id);
-
-      const className = clsx(
-        styles.walletButton,
-        buttonStyles.button,
-        buttonStyles.light,
-        buttonStyles.large,
-        buttonStyles.block,
-      );
 
       return (
         <li key={id}>
-          <button type="button" className={className} onClick={onClick} disabled={!isEnabled}>
+          <button
+            type="button"
+            className={clsx(
+              styles.walletButton,
+              buttonStyles.button,
+              buttonStyles.light,
+              buttonStyles.large,
+              buttonStyles.block,
+            )}
+            onClick={() => (isConnected ? setWalletId(id) : connect?.())}
+            disabled={!isEnabled}>
             <WalletItem icon={SVG} name={name} />
 
             <div className={styles.status}>
-              <p>{status}</p>
-              {isEnabled && <p>{accountsStatus}</p>}
+              <p>{isConnected ? 'Enabled' : 'Disabled'}</p>
+              {isConnected && <p>{accountsStatus}</p>}
             </div>
           </button>
         </li>
       );
     });
 
-  const getAccounts = () =>
+  const renderAccounts = () =>
     walletAccounts?.map((_account) => {
       const { address, meta } = _account;
       const isActive = address === account?.address;
 
       const handleClick = () => {
         login(_account);
-        saveWallet();
         onClose();
       };
 
@@ -98,26 +97,50 @@ function WalletModal({ onClose }: Props) {
 
   const handleLogoutButtonClick = () => {
     logout();
-    removeWallet();
     onClose();
   };
 
   const walletButtonClassName = clsx(buttonStyles.button, buttonStyles.transparent);
 
+  const renderFooter = () =>
+    wallet && (
+      <div className={styles.footer}>
+        <button type="button" className={walletButtonClassName} onClick={resetWalletId}>
+          <WalletItem icon={wallet.SVG} name={wallet.name} />
+
+          <span className={styles.changeText}>Change</span>
+        </button>
+
+        {account && <Button icon={ExitSVG} text="Logout" color="transparent" onClick={handleLogoutButtonClick} />}
+      </div>
+    );
+
   return (
-    <Modal heading="Wallet connection" close={onClose}>
-      <ul className={styles.list}>{getAccounts() || getWallets()}</ul>
+    <Modal heading="Wallet connection" close={onClose} footer={renderFooter()}>
+      {isAnyWallet ? (
+        <>
+          {!wallet && <ul className={styles.list}>{renderWallets()}</ul>}
 
-      {wallet && (
-        <footer className={styles.footer}>
-          <button type="button" className={walletButtonClassName} onClick={resetWalletId}>
-            <WalletItem icon={wallet.SVG} name={wallet.name} />
-
-            <span className={styles.changeText}>Change</span>
-          </button>
-
-          {account && <Button icon={ExitSVG} text="Logout" color="transparent" onClick={handleLogoutButtonClick} />}
-        </footer>
+          {!!wallet &&
+            (walletAccounts?.length ? (
+              <ul className={styles.list}>{renderAccounts()}</ul>
+            ) : (
+              <p>No accounts found. Please open your extension and create a new account or import existing.</p>
+            ))}
+        </>
+      ) : (
+        <p>
+          Wallet extensions were not found or disconnected. Please check how to install a supported wallet and create an
+          account{' '}
+          <a
+            href="https://wiki.gear-tech.io/docs/idea/account/create-account"
+            target="_blank"
+            rel="noreferrer"
+            className="link-text">
+            here
+          </a>
+          .
+        </p>
       )}
     </Modal>
   );
